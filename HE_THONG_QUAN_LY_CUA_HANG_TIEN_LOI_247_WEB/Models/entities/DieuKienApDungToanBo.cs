c00554087c5d7abb72bf78ebf85880125eeff6b6
trangkimdat2005 <trangkimdatst2005@gmail.com>
2025-11-02 19:40:54 +0700

    cập nhật database

---
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Models/entities/DieuKienApDungToanBo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Models.entities;
+
+public partial class DieuKienApDungToanBo
+{
+    public string Id { get; set; } = null!;
+
+    public string DieuKienId { get; set; } = null!;
+
+    public string? GhiChu { get; set; }
+
+    public bool IsDelete { get; set; }
+
+    public virtual DieuKienApDung DieuKien { get; set; } = null!;
+}
