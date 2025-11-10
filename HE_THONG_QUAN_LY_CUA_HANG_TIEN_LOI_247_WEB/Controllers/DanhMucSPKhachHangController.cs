bb7176ff35a07998e3066198df7f726975a0353b
trangkimdat2005 <trangkimdatst2005@gmail.com>
2025-11-10 16:25:47 +0700

    hehe

---
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Controllers/DanhMucSPKhachHangController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Controllers
+{
+    public class DanhMucSPKhachHangController : Controller
+    {
+        [Route("/DanhSachSanPhamKhachHang")]
+        public IActionResult DanhSachSanPhamKhachHang()
+        {
+            return View();
+        }
+    }
+}
--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Models/entities/KhachHang.cs
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Models/entities/KhachHang.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Models;
+namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Models.Entities;
 
 public partial class KhachHang
 {
