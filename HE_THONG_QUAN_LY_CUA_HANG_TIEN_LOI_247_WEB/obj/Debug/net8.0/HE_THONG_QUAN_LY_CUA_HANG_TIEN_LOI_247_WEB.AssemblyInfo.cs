f27e06184078c62f4a4018a5ffc8536d0afe8eff
LuuNghia <luudinhnghia30012005@gmail.com>
2025-11-10 16:55:45 +0700

    xong đường dẫn

--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/obj/Debug/net8.0/HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.AssemblyInfo.cs
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/obj/Debug/net8.0/HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd132922ba3b5a05c22a82fc1c60b07fa91b55a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb034665986701b1748f447231deec6eac81e0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
