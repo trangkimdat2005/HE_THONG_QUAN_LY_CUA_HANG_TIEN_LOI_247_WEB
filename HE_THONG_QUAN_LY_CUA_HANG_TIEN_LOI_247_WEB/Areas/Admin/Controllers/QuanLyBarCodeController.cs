4014eea4676cc9a965887262e2870f5f7b944e07
taplamcode <tranthanhngan.29.05.2005@gmail.com>
2025-11-13 23:59:28 +0700

    day du lieuu mau

--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Areas/Admin/Controllers/QuanLyBarCodeController.cs
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Areas/Admin/Controllers/QuanLyBarCodeController.cs
@@ -1,18 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Models.EF;
+using HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class QuanLyBarCodeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public QuanLyBarCodeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [Route("/QuanLyBarCode/DanhSachTemNhan")]
         public IActionResult DanhSachTemNhan()
         {
+            // Lấy danh sách tem nhãn với thông tin liên quan
+            var lstTemNhan = _context.TemNhans
+                .Where(t => !t.IsDelete)
+                .Include(t => t.MaDinhDanh)
+                    .ThenInclude(m => m.SanPhamDonVi)
+                        .ThenInclude(sp => sp.SanPham)
+                .Include(t => t.MaDinhDanh)
+                    .ThenInclude(m => m.SanPhamDonVi)
+                        .ThenInclude(sp => sp.DonVi)
+                .OrderByDescending(t => t.NgayIn)
+                .AsNoTracking()
+                .ToList();
+
+            // Lấy danh sách mã định danh để chọn khi tạo tem mới
+            var lstMaDinhDanh = _context.MaDinhDanhSanPhams
+                .Where(m => !m.IsDelete)
+                .Include(m => m.SanPhamDonVi)
+                    .ThenInclude(sp => sp.SanPham)
+                .Include(m => m.SanPhamDonVi)
+                    .ThenInclude(sp => sp.DonVi)
+                .AsNoTracking()
+                .ToList();
+
+            ViewData["lstTemNhan"] = lstTemNhan;
+            ViewData["lstMaDinhDanh"] = lstMaDinhDanh;
+
             return View();
         }
+
         [Route("/QuanLyBarCode/DinhDanhSanPham")]
         public IActionResult DinhDanhSanPham()
         {
+            // Lấy danh sách mã định danh sản phẩm
+            var lstMaDinhDanh = _context.MaDinhDanhSanPhams
+                .Where(m => !m.IsDelete)
+                .Include(m => m.SanPhamDonVi)
+                    .ThenInclude(sp => sp.SanPham)
+                .Include(m => m.SanPhamDonVi)
+                    .ThenInclude(sp => sp.DonVi)
+                .OrderBy(m => m.SanPhamDonVi.SanPham.Ten)
+                .AsNoTracking()
+                .ToList();
+
+            // Lấy danh sách sản phẩm đơn vị để chọn khi tạo mã mới
+            var lstSanPhamDonVi = _context.SanPhamDonVis
+                .Where(sp => !sp.IsDelete)
+                .Include(sp => sp.SanPham)
+                .Include(sp => sp.DonVi)
+                .OrderBy(sp => sp.SanPham.Ten)
+                .AsNoTracking()
+                .ToList();
+
+            ViewData["lstMaDinhDanh"] = lstMaDinhDanh;
+            ViewData["lstSanPhamDonVi"] = lstSanPhamDonVi;
+
             return View();
         }
     }
--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Areas/Admin/Controllers/QuanLyDonHangController.cs
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Areas/Admin/Controllers/QuanLyDonHangController.cs
@@ -1,18 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Models.EF;
+using HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class QuanLyDonHangController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public QuanLyDonHangController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [Route("/QuanLyDonHang/DanhSachDonHang")]
         public IActionResult DanhSachDonHang()
         {
+            // Lấy danh sách hóa đơn
+            var lstHoaDon = _context.HoaDons
+                .Where(hd => !hd.IsDelete)
+                .Include(hd => hd.NhanVien)
+                .Include(hd => hd.KhachHang)
+                .OrderByDescending(hd => hd.NgayLap)
+                .AsNoTracking()
+                .ToList();
+
+            ViewData["lstHoaDon"] = lstHoaDon;
+
             return View();
         }
+
         [Route("/QuanLyDonHang/XemChiTietHoaDon")]
-        public IActionResult XemChiTietHoaDon()
+        public IActionResult XemChiTietHoaDon(string id)
         {
+            // Lấy thông tin hóa đơn
+            var hoaDon = _context.HoaDons
+                .Where(hd => hd.Id == id && !hd.IsDelete)
+                .Include(hd => hd.NhanVien)
+                .Include(hd => hd.KhachHang)
+                .Include(hd => hd.ChiTietHoaDons)
+                    .ThenInclude(ct => ct.SanPhamDonVi)
+                        .ThenInclude(sp => sp.SanPham)
+                .Include(hd => hd.ChiTietHoaDons)
+                    .ThenInclude(ct => ct.SanPhamDonVi)
+                        .ThenInclude(sp => sp.DonVi)
+                .Include(hd => hd.GiaoDichThanhToans)
+                    .ThenInclude(gd => gd.KenhThanhToan)
+                .AsNoTracking()
+                .FirstOrDefault();
+
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["HoaDon"] = hoaDon;
+
             return View();
         }
     }
