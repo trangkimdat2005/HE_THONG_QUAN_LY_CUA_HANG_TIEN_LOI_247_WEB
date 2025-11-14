e729d17df7619455f2fb698b858100cd8c6b41e5
LuuNghia <luudinhnghia30012005@gmail.com>
2025-11-14 20:35:53 +0700

    xem chi tiết nhập kho

--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Services/ITonKhoServices.cs
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Services/ITonKhoServices.cs
@@ -6,5 +6,10 @@ namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Services
     {
         PhieuNhap GetPhieuNhapById(string id);
         List<ChiTietPhieuNhap> GetChiTietPhieuNhap(string phieuNhapId);
+        
+        PhieuXuat GetPhieuXuatById(string id);
+        List<ChiTietPhieuXuat> GetChiTietPhieuXuat(string phieuXuatId);
+
+        KiemKe GetKiemKeById(string id);
     }
 }
