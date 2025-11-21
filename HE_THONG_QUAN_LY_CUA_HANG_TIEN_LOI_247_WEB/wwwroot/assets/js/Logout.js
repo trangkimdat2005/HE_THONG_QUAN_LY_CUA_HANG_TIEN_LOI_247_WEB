caf587918a4753ada5774d26224096ae9e5ead13
trangkimdat2005 <trangkimdatst2005@gmail.com>
2025-11-22 00:12:37 +0700

    sssddzcxz

--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/wwwroot/assets/js/Logout.js
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/wwwroot/assets/js/Logout.js
@@ -1,19 +1,20 @@
-﻿
-$(function () {
+﻿$(function () {
     $('#logout-user').on('click', function (e) {
-        e.preventDefault();  // Ngừng hành động mặc định của thẻ a
+        e.preventDefault();
 
-        // Gửi yêu cầu POST để đăng xuất
         $.ajax({
-            url: "/Account/Logout", // Địa chỉ của action Logout trong controller Account
+            url: '/Account/Logout',
             type: 'POST',
+            data: {
+                __RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val()
+            },
             success: function (response) {
-                // Chuyển hướng đến trang đăng nhập hoặc trang chủ sau khi đăng xuất thành công
                 if (response.status === 'SUCCESS') {
-                    window.location.href = response.redirectUrl; // Chuyển hướng
+                    window.location.href = response.redirectUrl;
                 }
             },
-            error: function () {
+            error: function (xhr) {
+                console.log(xhr.responseText); // Xem chi tiết lỗi
                 alert("Có lỗi xảy ra khi đăng xuất.");
             }
         });
