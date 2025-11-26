9f8d75f872e767e5585bce7b32d9961230507667
trangkimdat2005 <trangkimdatst2005@gmail.com>
2025-11-26 17:01:25 +0700

    đ

--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Controllers/LoiDangNhapController.cs
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/Controllers/LoiDangNhapController.cs
@@ -8,5 +8,10 @@ namespace HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB.Controllers
         {
             return View();
         }
+
+        public IActionResult KhongCoQuyen()
+        {
+            return View();
+        }
     }
 }
---
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WEB/wwwroot/assets/js/KhongCoQuyen.js
@@ -0,0 +1,10 @@
+﻿function goBack() {
+    // Kiểm tra xem có lịch sử duyệt web không
+    if (document.referrer) {
+        window.history.back();
+    } else {
+        // Nếu không có lịch sử (mở tab mới), quay về trang chủ
+        alert("Không tìm thấy đường về trang trước, mình sẽ đưa bạn về trang chủ nhé!");
+        window.location.href = '/'; // Thay đổi đường dẫn này thành trang chủ của bạn
+    }
+}
